787b620106156e63eb83dfe6b7f49f14f0ac0056
Christopher Bertels <bakkdoor@flasht.de>
2008-07-20 19:45:11 +0200

    tags added

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -9,7 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080711163033) do
+ActiveRecord::Schema.define(:version => 20080720171103) do
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "team_memberships", :force => true do |t|
     t.integer  "user_id"
