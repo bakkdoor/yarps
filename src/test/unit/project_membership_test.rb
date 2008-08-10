82fd1a92648a86d349b89faad2fb0c8de2e506ba
Christopher Bertels <bakkdoor@flasht.de>
2008-08-10 04:33:41 +0200

    test files updated

--- src/test/unit/project_membership_test.rb
+++ src/test/unit/project_membership_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class TeamTest < ActiveSupport::TestCase
+class ProjectMembershipTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   def test_truth
     assert true
--- src/test/unit/project_test.rb
+++ src/test/unit/project_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class TeamMembershipTest < ActiveSupport::TestCase
+class ProjectTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   def test_truth
     assert true
