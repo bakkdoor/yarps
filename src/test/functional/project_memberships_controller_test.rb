4df343b978c3bae91ade087891b33c7701faecaf
Christopher Bertels <bakkdoor@flasht.de>
2008-08-23 01:13:30 +0200

    fehler behoben

--- src/test/functional/project_memberships_controller_test.rb
+++ src/test/functional/project_memberships_controller_test.rb
@@ -21,23 +21,23 @@ class ProjectMembershipsControllerTest < ActionController::TestCase
   end
 
   def test_should_show_project_membership
-    get :show, :id => project_memberships(:one).id
+    get :show, :id => project_memberships(:non_private_and_none_invite_only).id
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :id => project_memberships(:one).id
+    get :edit, :id => project_memberships(:non_private_and_none_invite_only).id
     assert_response :success
   end
 
   def test_should_update_project_membership
-    put :update, :id => project_memberships(:one).id, :project_membership => { }
+    put :update, :id => project_memberships(:non_private_and_none_invite_only).id, :project_membership => { }
     assert_redirected_to project_membership_path(assigns(:project_membership))
   end
 
   def test_should_destroy_project_membership
     assert_difference('ProjectMembership.count', -1) do
-      delete :destroy, :id => project_memberships(:one).id
+      delete :destroy, :id => project_memberships(:non_private_and_none_invite_only).id
     end
 
     assert_redirected_to project_memberships_path
--- src/test/functional/projects_controller_test.rb
+++ src/test/functional/projects_controller_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
-class TeamsControllerTest < ActionController::TestCase
+class ProjectsControllerTest < ActionController::TestCase
   def test_should_get_index
     get :index
     assert_response :success
-    assert_not_nil assigns(:teams)
+    assert_not_nil assigns(:projects)
   end
 
   def test_should_get_new
@@ -12,34 +12,34 @@ class TeamsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  def test_should_create_team
-    assert_difference('Team.count') do
-      post :create, :team => { }
+  def test_should_create_project
+    assert_difference('project.count') do
+      post :create, :project => { }
     end
 
-    assert_redirected_to team_path(assigns(:team))
+    assert_redirected_to project_path(assigns(:project))
   end
 
-  def test_should_show_team
-    get :show, :id => teams(:one).id
+  def test_should_show_project
+    get :show, :id => projects(:non_private_and_none_invite_only).id
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :id => teams(:one).id
+    get :edit, :id => projects(:non_private_and_none_invite_only).id
     assert_response :success
   end
 
-  def test_should_update_team
-    put :update, :id => teams(:one).id, :team => { }
-    assert_redirected_to team_path(assigns(:team))
+  def test_should_update_project
+    put :update, :id => projects(:non_private_and_none_invite_only).id, :project => { }
+    assert_redirected_to project_path(assigns(:project))
   end
 
-  def test_should_destroy_team
-    assert_difference('Team.count', -1) do
-      delete :destroy, :id => teams(:one).id
+  def test_should_destroy_project
+    assert_difference('project.count', -1) do
+      delete :destroy, :id => projects(:non_private_and_none_invite_only).id
     end
 
-    assert_redirected_to teams_path
+    assert_redirected_to projects_path
   end
 end
