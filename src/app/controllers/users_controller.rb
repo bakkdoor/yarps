645bda6dd3de63cd176e9e111c09f6afd07aa485
Christopher Bertels <bakkdoor@flasht.de>
2008-09-04 00:38:47 +0200

    generated new auth-system via restful_authentication plugin

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,29 +1,74 @@
 class UsersController < ApplicationController
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
   
-  auto_complete_for :user, :login
+  # Protect these actions behind an admin login
+  # before_filter :admin_required, :only => [:suspend, :unsuspend, :destroy, :purge]
+  before_filter :find_user, :only => [:suspend, :unsuspend, :destroy, :purge]
   
-  # search for user-profiles or so... 
-  def index
-    @users = User.find(:all)
+
+  # render new.rhtml
+  def new
+    @user = User.new
   end
-  
-  # profile-page of an account
-  def show
-    begin
-      @profile_user = User.find(params[:id])
-    rescue
-      flash[:error] = "Profile for UserID '#{params[:id]}' has not been found. "
-      redirect_to :controller => :users, :action => :index  
+ 
+  def create
+    logout_keeping_session!
+    @user = User.new(params[:user])
+    @user.register! if @user && @user.valid?
+    success = @user && @user.valid?
+    if success && @user.errors.empty?
+      redirect_back_or_default('/')
+      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+    else
+      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      render :action => 'new'
     end
   end
-  
-  def auto_complete_for_user_login
-    @users = User.find(:all, 
-      :conditions => [ 'LOWER(login) LIKE ?',
-      '%' + params[:user][:login].downcase + '%' ], 
-      :order => 'login ASC',
-      :limit => 10)
-    render :partial => 'auto_complete_results'
+
+  def activate
+    logout_keeping_session!
+    user = User.find_by_activation_code(params[:activation_code]) unless params[:activation_code].blank?
+    case
+    when (!params[:activation_code].blank?) && user && !user.active?
+      user.activate!
+      flash[:notice] = "Signup complete! Please sign in to continue."
+      redirect_to '/login'
+    when params[:activation_code].blank?
+      flash[:error] = "The activation code was missing.  Please follow the URL from your email."
+      redirect_back_or_default('/')
+    else 
+      flash[:error]  = "We couldn't find a user with that activation code -- check your email? Or maybe you've already activated -- try signing in."
+      redirect_back_or_default('/')
+    end
+  end
+
+  def suspend
+    @user.suspend! 
+    redirect_to users_path
+  end
+
+  def unsuspend
+    @user.unsuspend! 
+    redirect_to users_path
+  end
+
+  def destroy
+    @user.delete!
+    redirect_to users_path
+  end
+
+  def purge
+    @user.destroy
+    redirect_to users_path
   end
   
+  # There's no page here to update or destroy a user.  If you add those, be
+  # smart -- make sure you check that the visitor is authorized to do so, that they
+  # supply their old password along with a new one to update it, etc.
+
+protected
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
     
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -55,7 +63,12 @@ ActionController::Routing::Routes.draw do |map|
                  
   map.resources 'project_memberships'
   
-  map.resources :users, :collection => {:auto_complete_for_user_login => :get }
+  map.resources :users, :member => { :suspend => :put, :unsuspend => :put, :purge => :delete }, :collection => {:auto_complete_for_user_login => :get }
+  
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.login  '/login',  :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
   
   map.connect '/de', :controller => 'home', :action => 'index', :language => 'de'
   map.connect '/en', :controller => 'home', :action => 'index', :language => 'en'
